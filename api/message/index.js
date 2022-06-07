0706ed1595943b7cb05e74e45aada351ea1bd51a
Jeremiah <t-jlipscomb@microsoft.com>
2022-06-07 16:03:36 -0700

    updated message functoin

--- api/message/index.js
+++ api/message/index.js
@@ -6,9 +6,8 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-        module.exports = async function (context, req) {
-            context.res.json({
-                text: "Hello from the API"
-            });
-        };
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        text: "Hello from the API"
+    };
 }
